0b0092d5bc81b6cff675ff96ae99102ff9a6d56b
Troy Prelog <35702532+tprelog@users.noreply.github.com>
2021-02-10 23:35:32 -0500

    :pencil2: rename scripts and use bash

--- post_install.sh
+++ post_install.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 sysrc zwavejs2mqtt_app_dir=/usr/local/share/zwavejs2mqtt
 sysrc zwavejs2mqtt_store_dir=/usr/local/etc/zwavejs2mqtt
 
-/root/bin/z2m_install
+/root/bin/z2m-install
 
 sysrc zwavejs2mqtt_enable="YES"
 service zwavejs2mqtt start
