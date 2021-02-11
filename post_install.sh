539e26293780e589768a8152a1a19f7669b723aa
Troy Prelog <35702532+tprelog@users.noreply.github.com>
2021-02-10 23:42:17 -0500

    :computer: add console menu

--- post_install.sh
+++ post_install.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
+## Configure the jail's primary service using rcvars
 sysrc zwavejs2mqtt_app_dir=/usr/local/share/zwavejs2mqtt
 sysrc zwavejs2mqtt_store_dir=/usr/local/etc/zwavejs2mqtt
 
+## Install the jail's primary service, zwavejs2mqtt
 /root/bin/z2m-install
 
+## Enable and start the service
 sysrc zwavejs2mqtt_enable="YES"
 service zwavejs2mqtt start
+
+## Start the console menu, upon login as user "root"
+echo -e "\n# Start console menu after login." >> /root/.login
+echo "if ( -x /root/bin/menu ) menu" >> /root/.login
