18211f631109c083009b01bb63779cfc960ecb10
Troy Prelog <35702532+tprelog@users.noreply.github.com>
2021-10-31 10:06:44 -0700

    :pencil2: disable version check

--- pre_update.sh
+++ pre_update.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
 # shellcheck disable=SC1091
-. /etc/rc.subr && load_rc_config
+#. /etc/rc.subr && load_rc_config
 
 # shellcheck disable=SC2154
-version="${plugin_version%%.*}"
-if [ "${version}" == '1' ] || [ "${version}" == '2' ]; then
-  exit 0
-else
-  err 1 "unknown plugin version"
-fi
+#version="${plugin_version%%.*}"
+#if [ "${version}" == '1' ] || [ "${version}" == '2' ]; then
+#  exit 0
+#else
+#  err 1 "unknown plugin version"
+#fi
+
+exit 0
